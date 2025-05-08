4fdba79c4c33df707eef9118331eface9c76e559
Paul Ramnora <pramnora@yahoo.com>
2025-05-09 00:19:34 +0100

    Update app1.py
    Re-wrote the previous code a bit. The output when the button is 'clicked', now says: 'Welcome to my Streamlit app.'

--- test/app1.py
+++ test/app1.py
@@ -2,5 +2,6 @@ import streamlit as st
 st.title("My first stream lit app.")
 
 button1=st.button("Click me")
+
 if button1:
-   st.write("This is some text.")
+   st.write("Welcome to my Streamlit page.")
