479bb823ac163854de9f7557625d62ede29e92d8
Paul Ramnora <pramnora@yahoo.com>
2026-06-04 19:36:17 +0100

    Refine comments in 12xtables.py
    Updated comments for clarity and consistency.

--- programs/12xtables.py
+++ programs/12xtables.py
@@ -1,11 +1,13 @@
 # CREATED: Date/Time: Thu 4th June 2026 19:30 PM GMT
 # UPDATED: Date/Time: Thu 4th June 2026 19:30 PM GMT
 # ------
+# Streamlit program: 12 x Tables
+# ------
 # This code shows how to use the 'slider' control
-# which can be linked to a variable;
+# to select a number from: 1 up to 12;
 # in fact, it uses 2 'slider' controls...
-# to control both variable: x,y;
-# then, then, produces the output sum of: x * y;
+# the slider control number values are stored in variables: x,y;
+# which, then, produces the output sum of: x * y;
 # or, in other words, produces the 12 x Tables.
 # ------
 import streamlit as st
