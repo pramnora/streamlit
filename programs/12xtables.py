4734ff1043f3310d89e9ba271657aecccc4af287
Paul Ramnora <pramnora@yahoo.com>
2026-06-04 19:31:46 +0100

    Add documentation comments for 12xtables.py
    Added comments to explain the purpose of the program.

--- programs/12xtables.py
+++ programs/12xtables.py
@@ -1,3 +1,13 @@
+# CREATED: Date/Time: Thu 4th June 2026 19:30 PM GMT
+# UPDATED: Date/Time: Thu 4th June 2026 19:30 PM GMT
+# ------
+# This code shows how to use the 'slider' control
+# which can be linked to a variable;
+# in fact, it uses 2 'slider' controls...
+# to control both variable: x,y;
+# then, then, produces the output sum of: x * y;
+# or, in other words, produces the 12 x Tables.
+# ------
 import streamlit as st
 st.header("PROGRAM: 12 Time tables:-")
 x=st.slider("Choose a x value:",1,12)
