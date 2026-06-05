ce2886a32e35c913ac41836c5f348de75e3f9c80
Paul Ramnora <pramnora@yahoo.com>
2026-06-05 11:22:58 +0100

    Add header with rainbow divider to Streamlit app

--- language/text/hw/hw01.py
+++ language/text/hw/hw01.py
@@ -1,3 +1,9 @@
 import streamlit as st
+
+# write header with multi-coloured underline divider showing at the bottom...
+
 st.header("My 1st Streamlit app.",divider="rainbow")
+
+# write some simple text...
+
 st.write("Hello, world!")
