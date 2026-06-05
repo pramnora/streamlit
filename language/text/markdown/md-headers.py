c931b65c9cd36bac3e92a436bdf939a520298a46
Paul Ramnora <pramnora@yahoo.com>
2026-06-05 11:26:18 +0100

    Add markdown header examples in md-headers.py

---
+++ language/text/markdown/md-headers.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.markdown("# Hello, world!")      # Level 1 header text
+st.markdown("## Hello, world!")     # Level 2 header text
+st.markdown("### Hello, world!")    # Level 3 header text
+st.markdown("#### Hello, world!")   # Level 4 header text
+st.markdown("##### Hello, world!")  # Level 5 header text
+st.markdown("###### Hello, world!") # Level 6 header text
