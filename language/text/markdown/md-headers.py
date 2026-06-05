1039a7b6a7cd12ca05e1e2da349a26780128fcfe
Paul Ramnora <pramnora@yahoo.com>
2026-06-05 18:03:48 +0100

    Update markdown headers to new text

--- language/text/markdown/md-headers.py
+++ language/text/markdown/md-headers.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
-st.markdown("# Hello, world!")      # Level 1 header text
-st.markdown("## Hello, world!")     # Level 2 header text
-st.markdown("### Hello, world!")    # Level 3 header text
-st.markdown("#### Hello, world!")   # Level 4 header text
-st.markdown("##### Hello, world!")  # Level 5 header text
-st.markdown("###### Hello, world!") # Level 6 header text
+st.markdown("# Header size 1!")      # Level 1 header text
+st.markdown("## Header size 2!")     # Level 2 header text
+st.markdown("### Header size 3!")    # Level 3 header text
+st.markdown("#### Header size 4!")   # Level 4 header text
+st.markdown("##### Header size 5!")  # Level 5 header text
+st.markdown("###### Header size 6!") # Level 6 header text
+
