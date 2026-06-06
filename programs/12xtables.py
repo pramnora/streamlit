244200d2851d94a9c4d2ca90b722512cb520ebb3
Paul Ramnora <pramnora@yahoo.com>
2026-06-06 16:55:38 +0100

    Update output formatting in 12xtables.py

--- programs/12xtables.py
+++ programs/12xtables.py
@@ -16,4 +16,4 @@ import streamlit as st
 st.header("PROGRAM: 12 Time tables:-")
 x=st.slider("Choose a x value:",1,12)
 y=st.slider("Choose a y value:",1,12)
-st.write("Output values: ( :red[***x***] : ",x,") ( :blue[***y***] : ",y,") ( :red[x] * :blue[y] : ",x*y,")")
+st.write("Output values: ( :red[***x***] = ",x,") ( :blue[***y***] = ",y,") ( :red[x] * :blue[y] = ",x*y,")")
