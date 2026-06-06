7b01dbcfdc5d0a48b6cad29bd3df64d59397d21a
Paul Ramnora <pramnora@yahoo.com>
2026-06-06 16:54:16 +0100

    Add comments to explain 12 x Tables program

--- programs/12xtables.py
+++ programs/12xtables.py
@@ -5,8 +5,10 @@
 # ------
 # This code shows how to use the 'slider' control
 # to select a number from: 1 up to 12;
+
 # in fact, it uses 2 such similar 'slider' controls...
 # the slider control number values are stored in variables: x,y;
+
 # which, then, produces the output sum of: x * y;
 # or, in other words, produces the 12 x Tables.
 # ------
