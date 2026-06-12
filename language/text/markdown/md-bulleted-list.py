0fc8d1c3bdaf891aa251ea492ba7babcf519df3b
Paul Ramnora <pramnora@yahoo.com>
2026-06-12 12:07:24 +0100

    Update markdown note on quote marks usage
    Added note about quote marks and line breaks.

--- language/text/markdown/md-bulleted-list.py
+++ language/text/markdown/md-bulleted-list.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+# ------------------------------------------------------------
+# NOTE: Using single or double quote marks...doesn't work;
+#       only triple quotes knows how to interpret line breaks.
+# ------------------------------------------------------------
+
 st.markdown("""
   This is a bulleted list...
   - Item 1
